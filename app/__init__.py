15ed93b8815cd18004da62d0edfa47305e0617cd
Jaime Gago <contact@jaimegago.com>
2015-10-30 00:33:08 +0000

    Use json config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,28 @@
 from flask import Flask
+import json
+import sys
 
 app = Flask(__name__)
-app.config.from_object('config')
+
+# TODO
+# Look for config files in multiple places
+# Handle prod vs dev config
+
+VITREEN_CONF_FILE = './vitreen-config.json'
+try:
+  with open(VITREEN_CONF_FILE, 'r') as f:
+    json_config = json.load(f)
+except Exception as e:
+  print 'Could not load config file: %s' % VITREEN_CONF_FILE
+  print e
+  sys.exit(1)
+
+for key in json_config:
+  if key.isupper() != True:
+    print 'all config keys in %s must be UPPERCASE!' % VITREEN_CONF_FILE
+    print '%s is not all UPPERCASE!' % key
+    sys.exit(1)
+
+app.config.update(json_config)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -4,31 +4,31 @@ from app import app
 from .forms import CreateEventForm
 import requests
 import json
+import urlparse
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/create_event', methods=['GET', 'POST'])
 def create_event():
-    form = CreateEventForm()
-    if form.validate_on_submit():
-      create_event_url = 'http://10.90.0.128/events/'
-      event_payload = {'what': form.title.data,
-          'tags': form.tags.data,
-          'data': form.desc.data}
-      post_request = requests.post('http://10.90.0.128/events/',
-          data=json.dumps(event_payload))
-      flash('Creating graphite event with tags="%s", title=%s description=%s' % (
-          form.tags.data, form.title.data, form.desc.data))
-      return redirect(url_for('index'))
-    return render_template('create_event.html',form=form)
+  form = CreateEventForm()
+  if form.validate_on_submit():
+    event_payload = {'what': form.title.data, 'tags': form.tags.data,
+        'data': form.desc.data}
+    post_request = requests.post(
+        urlparse.urljoin(app.config['GRAPHITE_SERVER_URL'], 'events/'),
+        data=json.dumps(event_payload)
+      )
+    flash('Creating graphite event with tags="%s", title=%s description=%s' % (
+      form.tags.data, form.title.data, form.desc.data))
+    return redirect(url_for('index'))
+  return render_template('create_event.html',form=form)
 
 @app.route('/events')
 def events():
-    events_data = requests.get('http://10.90.0.128/events/get_data')
-    events = events_data.json()
-    return render_template('events_listing.html', events=events)
-
-
+  events_data = requests.get(urlparse.urljoin(app.config['GRAPHITE_SERVER_URL'],
+    'events/get_data'))
+  events = events_data.json()
+  return render_template('events_listing.html', events=events)
