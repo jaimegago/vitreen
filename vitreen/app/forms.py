5ec824808faa935a62fbe43214cc2d55bd22ca0d
Sean Clemmer <sczizzo@gmail.com>
2015-11-10 15:08:25 -0800

    Add "when" DateTime input field

--- vitreen/app/forms.py
+++ vitreen/app/forms.py
@@ -1,9 +1,18 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
+from wtforms.fields.html5 import DateTimeField
 from wtforms.validators import DataRequired
+from wtforms_components import DateRange
+from datetime import datetime
 
 class CreateEventForm(Form):
   tags  = StringField('Tags', validators=[DataRequired()])
   title = StringField('Title', validators=[DataRequired()])
   desc  = StringField('Description', validators=[DataRequired()])
+  when  = DateTimeField('When', default=datetime.now(), validators=[
+    DataRequired(), DateRange(
+        min=datetime(2000, 1, 1),
+        max=datetime.now()
+      )
+    ])
   submit = SubmitField('Submit')
